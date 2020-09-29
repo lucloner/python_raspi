0395dc712c4ac379d6cfa58a1a1bd7be7c9644c2
biggee@car <lucloner@hotmail.com>
2020-09-29 20:03:51 +0800

    prevent emergency mode

--- pyGpsd.sh
+++ pyGpsd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo !!!===try ping===!!! > /home/pi/src/pyGpsd.log
+echo !!!===try ping===!!! > /media/pi/U/pyGpsd.log
 while [ -s $(ping -4 -c1 114.114.114.114 | grep '^rtt' | awk '{print $4}'| awk -F'/' '{print $3F}'| awk -F'.' '{print $1F}')]
 do
 	echo waiting
@@ -7,7 +7,7 @@ do
 done
 while :
 do
-	/usr/bin/python3 /home/pi/src/pyGpsd.py >> /home/pi/src/pyGpsd.log
-	echo !!!===end===!!! >> /home/pi/src/pyGpsd.log
+	/usr/bin/python3 /home/pi/src/pyGpsd.py # > /media/pi/U/pyGpsd.log
+	echo !!!===end===!!! >> /media/pi/U/pyGpsd.log
 	sleep 1
 done
